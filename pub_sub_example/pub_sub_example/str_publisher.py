4e4b5ee4bb7e0806631dc8fb192350eccec9df48
Brett Nguyen <brettnguyen.engr@gmail.com>
2026-03-15 17:21:02 -0400

    set up StringPublisher class

--- pub_sub_example/pub_sub_example/str_publisher.py
+++ pub_sub_example/pub_sub_example/str_publisher.py
@@ -2,6 +2,14 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
+class StringPublisher(Node):
+    def __init__(self):
+        super().__init__('str_publisher')
+        self.publisher_ = self.create_publisher(String, 'chatter', 10)
+        timer_period = 0.5 # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
 def main():
     print('Hi from pub_sub_example.')
 
