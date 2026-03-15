d0a7ff797650c720ee8b23e62f00e284402ce612
Brett Nguyen <brettnguyen.engr@gmail.com>
2026-03-15 15:34:42 -0400

    initial commit

---
+++ pub_sub_example/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'pub_sub_example'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='brett',
+    maintainer_email='brett@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'str_publisher = pub_sub_example.str_publisher:main'
+        ],
+    },
+)
