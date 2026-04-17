45037a6bcd301997d8d83acd0903528ff40a290c
Brett Nguyen <brettnguyen.engr@gmail.com>
2026-04-17 11:04:29 -0400

    remove old code

--- romi_twist_control/romi_twist_control/i2c_node.py
+++ romi_twist_control/romi_twist_control/i2c_node.py
@@ -3,9 +3,6 @@ from rclpy.node import Node
 from std_msgs.msg import Int16MultiArray, Int32MultiArray
 from .a_star import AStar
 
-# Import the custom WheelSpeeds message
-# from romi_interfaces.msg import WheelSpeeds
-
 class I2CBridgeNode(Node):
     def __init__(self):
         super().__init__('i2c_bridge')
@@ -31,33 +28,10 @@ class I2CBridgeNode(Node):
         # Read encoders at 50Hz
         self.timer = self.create_timer(0.02, self.read_encoders)
         
-
-        # Create the subscriber for the wheel speeds
-        # NOTE: We are commenting out the actual subscription until we build the msg file
-        # self.subscription = self.create_subscription(
-        #     WheelSpeeds,
-        #    '/wheel_speeds',
-        #    self.speeds_callback,
-        #    10
-        #)
-        
         # Safety: Ensure motors start at 0
         self.romi.motors(0, 0)
 
         self.get_logger().info("AStar I2C Interface Initialized.")
-
-    # def speeds_callback(self, msg):
-        # Extract the speeds from the message
-        # The 32U4 expects 16-bit integers (roughly -300 to 300 for raw PWM)
-    #    left_speed = int(msg.left)
-    #    right_speed = int(msg.right)
-        
-    #    try:
-            # Send the speeds over I2C using Pololu's function
-    #        self.romi.motors(left_speed, right_speed)
-    #    except OSError as e:
-            # I2C can occasionally drop packets, it's good practice to catch this
-    #        self.get_logger().error(f"I2C Communication Error: {e}")
 
     def pwm_callback(self, msg):
         left_pwm = int(msg.data[0])
